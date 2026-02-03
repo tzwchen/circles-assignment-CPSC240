905efba7f799559b8acda8c05cc5931fd9ef3428
t <tristanandkauai@icloud.com>
2026-02-03 10:22:07 -0800

    implemented the driver program

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 extern "C" {
-    double circle(double radius);
+    double circle();
     int isfloat(const char* str);
 }
 
 int main() {
     cout << "Welcome to circles." << endl;
     cout << "This program was created and is maintained by Tristan Chen." << endl;
-    
-    
+    circle();
+    return 0;
     
 }
