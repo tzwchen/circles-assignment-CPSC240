3d85e76e53d89961ae4ddf85f8e4bd3f649ae598
t <tristanandkauai@icloud.com>
2026-02-03 19:52:10 -0800

    added licenses

--- isfloat.asm
+++ isfloat.asm
@@ -1,3 +1,25 @@
+/****************************************************************************************************************************
+//Program name: "Circles". The purpose of this program is to calculate the area of a circle based on user input radius.
+//Copyright (C) 2026 Tristan chen *
+// *
+//This file is part of the software program "Circles". *
+//"Circles" is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public *
+//License version 3 as published by the Free Software Foundation. *
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied *
+//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details. *
+//A copy of the GNU General Public License v3 has been distributed with this software. If not found it is available here: *
+//<https://www.gnu.org/licenses/>. The copyright holder may be contacted here: tchen2006@csu.fullerton.edu *
+//****************************************************************************************************************************/
+
+;Purpose of this file, isfloat.asm is to get a valid float number from user input. The function isfloat will
+;prompt the user to enter a float number, then it will read the input using scanf and validate it. If the input is valid, it will return the float 
+;number in xmm0 register.
+
+
+
 ;Begin code
 
 extern printf 
--- r.sh
+++ r.sh
@@ -1,3 +1,20 @@
+#/****************************************************************************************************************************
+#//Program name: "Circles". The purpose of this program is to calculate the area of a circle based on user input radius.
+#//Copyright (C) 2026 Tristan chen *
+#// *
+#//This file is part of the software program "Circles". *
+#//"Circles" is free software: you can redistribute it and/or modify it under the terms of the GNU General
+#Public *
+#//License version 3 as published by the Free Software Foundation. *
+#//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+#implied *
+#//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+#for more details. *
+#//A copy of the GNU General Public License v3 has been distributed with this software. If not found it is available here: *
+#//<https://www.gnu.org/licenses/>. The copyright holder may be contacted here: tchen2006@csu.fullerton.edu *
+#//****************************************************************************************************************************/
+
+
 #!/bin/bash
 echo "Compiling and running circle program..."
 
